2040854cecdff1bd03f7f213dc018cb57eeff249
meesa susmith <45920678+meesasusmith@users.noreply.github.com>
2024-04-30 09:52:42 -0500

    first

---
+++ src/components/Home/Data/Booking.js
@@ -0,0 +1,24 @@
+const BookData = [
+    {
+      id: 1,
+      name: "Destination 1",
+      flights: [
+        { id: 1, name: "Emirates", price: 100 },
+        { id: 2, name: "Frontier Airlines", price: 120 },
+        { id: 2, name: "American Airlines", price: 114 },
+        { id: 2, name: "Southwest Airlines", price: 119 },
+      ],
+      hotels: [
+        { id: 1, name: "Hotel 1", price: 80 },
+        { id: 2, name: "Hotel 2", price: 90 }
+      ],
+      guides: [
+        { id: 1, name: "Guide 1", price: 50 },
+        { id: 2, name: "Guide 2", price: 60 }
+      ]
+    },
+    // Add more destinations as needed
+  ];
+  
+  export default BookData;
+  
---
+++ src/components/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from '../Navbar/HomeNavbar';
+import Hero from './Hero';
+import Search from './Search';
+
+import Discover from './Discover';
+import About from './About';
+import Contact from './Contact'
+
+const Home = () => {
+  return (
+    <>
+        <Navbar />
+        <Hero />
+        <Search />
+
+        <Discover />
+        <About />
+        <Contact />
+    </>
+  );
+};
+
+export default Home;
